81b6ba51fa414af940f77b50e90c183f836b2a1a
wangxm17 <57248757+wangxm17@users.noreply.github.com>
2022-04-13 16:35:24 +0800

    ...

---
+++ src/main/java/com/vo/test.java
@@ -0,0 +1,15 @@
+package com.vo;
+
+import com.SpringdemoApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @Author: wangxm
+ * @Date: 2022/3/31 9:25
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("测试一下");
+        System.out.println("testhifweb");
+    }
+}
